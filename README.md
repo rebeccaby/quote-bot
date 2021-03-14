1516d41bf6ac1931842a4c2205bfe4e60f3d5f22
Rebecca Byun <rebecca_byun@hotmail.com>
2021-03-14 10:22:12 -0500

    Added  ping

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # To-Do:
-  1. Create and store "quote" attributes
-  2. Check number of arguments for $quote, and limit to <= 3
+- [ ] Create and store "quote" documents
+- [ ] ```$quote "quote"``` -> manually-written quote by author
+- [ ] ```$quote message-link``` -> linked message from user
+- [x] ```$quote @user``` -> last sent message from user
+- [ ] $help command
+- [ ] Have "title" field in each document
+- [ ] Scrollable embedded messages via emoting
 
-# Future features:
-
-  * $quote @user "quote" -> manually-written quote
-  * $quote @user -> last sent message from user
-  * $quote @user message-link -> linked message from user
+# Future:
   * error-checking for (1) user has sent a message before (2) a user is pinged after $quote
   * scroll through embedded quote cards with emotes (like Mudae)
+
+# Improbable:
+  * ```$quote @user "quote"``` -> manually-written quote by pinged user (2 args)
--- bot.py
+++ bot.py
@@ -3,10 +3,14 @@ from discord.ext import commands
 import pymongo
 from pymongo import MongoClient
 import urllib.parse
+import time
+import datetime
 
 # Establish client connection and choose command prefix
 client = commands.Bot(command_prefix="$")
 
+# Separating sensitive information from src
+token = open("token.txt").read()
 with open("cred.txt") as file:
     lines = [line.strip() for line in file]
 name = lines[0]
@@ -18,8 +22,8 @@ cluster = MongoClient(mongo_url)
 db = cluster["QuoteBot"]
 collection = db["QuoteBot"]
 
-# Fetch token id
-token = open("token.txt").read()
+# Global constants
+MSG_LIMIT = 10
 
 # Connect event handler
 @client.event
@@ -29,32 +33,49 @@ async def on_ready():
 # Quote command
 @client.command()
 async def quote(ctx, *args):
-    await ctx.channel.send("{} arguments: {}".format(len(args), ", ".join(args)))
+    await client.wait_until_ready()
+
+    #print("{} arguments: {}".format(len(args), ", ".join(args)))
+
+    if len(args) > 2:
+        await ctx.channel.send(f"<@{ctx.author.id}>, cannot process command!")
+    else:
+        if len(args) == 1:
+            # $quote <@user>
+            if args[0][0:3] == "<@!" and args[0][-1] == ">":
+                user_id = int(args[0][3:-1])
+                has_messaged = False
+                async for message in ctx.channel.history(limit=MSG_LIMIT):
+                    if message.author.id == user_id and not has_messaged:
+                        has_messaged = True
+                        print(message.created_at.strftime("%m/%d/%y @ %H:%M:%S %p"))
+                        print(f"\t[ {message.content} ]")
+                    elif message.author.id == user_id and has_messaged:
+                        break
+                    else:
+                        print("User hasn't sent any messages.")
+                
+                #await ctx.channel.history(limit=20).find(lambda m : m.author.id == user_id)
+
+            '''elif args[0].startswith("https://discord.com/channels/") is True:
+                message_link = args[0].split("/")
+
+                guild_id = int(message_link[4])
+                channel_id = int(message_link[6])
+                message_id = int(message_link[5])
+
+                guild = client.get_guild(guild_id)
+                channel = guild.get_channel(channel_id) # NoneType for some reason
+                message = await channel.fetch_message(message_id) <- returns NoneType 
+                await ctx.channel.send(message)'''
+
+        else:
+            await ctx.channel.send(f"<@{ctx.author.id}>, command received!")
 
 # Message event handler
 @client.event
 async def on_message(ctx):
     print(f"#{ctx.channel}: {ctx.author}: {ctx.content}")
-    
-    # Create query for user id
-    myquery = {"_id": ctx.author.id}
-    
-    # If entity doesn't already exist, create one
-    if (collection.count_documents(myquery) == 0):
-        if ctx.content.lower() == "ping":
-            post = {"_id": ctx.author.id, "score": 1}
-            collection.insert_one(post)
-            await ctx.channel.send("pong - 0")
-
-    # If entity exists, increment by 1
-    else:
-        if ctx.content.lower() == "ping":
-            query = {"_id": ctx.author.id}
-            user = collection.find(query)
-            for result in user:
-                score = result["score"]
-            collection.update_one({"_id": ctx.author.id}, {"$set": {"score": score + 1}})
-            await ctx.channel.send(f"pong - {score}")
 
     # Coroutine - triggers registered commands
     await client.process_commands(ctx)
